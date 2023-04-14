ab4c8db303dcae332b295320bea1214dce210d59
Jason Xu <Xinwen31415@gmail.com>
2023-04-14 11:55:58 -0500

    whoops

--- feaux-s/browser-api.h
+++ feaux-s/browser-api.h
@@ -2,6 +2,7 @@
 #define BROWSER_API_H
 
 #include "decls.h"
+#include "utils.h"
 
 extern void initOS(uint numCores, SchedulingStrategy strategy);
 extern void cleanupOS();
@@ -9,12 +10,17 @@ extern void cleanupOS();
 extern "C" {
 Instruction* exported allocInstructionList(uint instructionCount);
 void exported freeInstructionList(Instruction* ptr);
+char* exported allocString(unsigned int size);
+void exported freeString(char* str);
 
 uint exported addProcess(Instruction* instructionList, uint size, char* processName);
 void exported pause();
 void exported unpause();
 void exported setClockDelay(uint delay);
 void exported setSchedulingStrategy(SchedulingStrategy strategy);
+
+MachineStateCompat* exported getMachineState();
+OSStateCompat* exported getOSState();
 }
 
 #endif
--- feaux-s/utils.h
+++ feaux-s/utils.h
@@ -12,12 +12,4 @@
 
 void exportProcess(const Process& src, ProcessCompat& dest);
 
-extern "C" {
-char* exported allocString(unsigned int size);
-void exported freeString(char* str);
-
-MachineStateCompat* exported getMachineState();
-OSStateCompat* exported getOSState();
-}
-
 #endif
